b9c8c2d4e797a72a11d3b437ad255d858fa87d85
ME-019 <sourishpramanik507@gmail.com>
2026-01-22 16:12:01 +0530

    Create isPrime.c

---
+++ isPrime.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main(){
+    int i,x;
+    printf("enter x:");
+    scanf("%d",&x);
+    for(i=2;i<x;i++){
+        if((x%i)==0){
+        printf("not prime");
+        break;}
+        
+    }
+    if(x==i){
+        printf("prime");
+    }
+    return 0;
+}
