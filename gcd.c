78a8191fb48169532a5a1a7cd4fdcd30c1d7bc78
Sourish Pramanik <sourishpramanik507@gmail.com>
2026-02-05 15:26:41 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+  int main(){
+    int a,b,gcd;
+    printf("enter a number a:");
+    scanf("%d",&a);
+    printf("enter a number b:");
+    scanf("%d",&b);
+
+    for(int i=1; i<=a && i<=b;i++){
+        if( a%i==0 && b%i==0){
+            gcd=i;
+        }
+
+
+
+    }
+
+        printf("%d",gcd);
+
+        return 0;
+
+
+
+
+
+  }
